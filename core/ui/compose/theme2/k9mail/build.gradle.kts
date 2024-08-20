6157b9974f474717bbf9d89ac588f0743cff70d9
makienkovs <makienkovs@yandex.ru>
2024-08-20 10:32:10 +0300

    add new app icon

--- core/ui/compose/theme2/k9mail/build.gradle.kts
+++ core/ui/compose/theme2/k9mail/build.gradle.kts
@@ -5,6 +5,15 @@ plugins {
 android {
     namespace = "app.k9mail.core.ui.compose.theme2.k9mail"
     resourcePrefix = "core_ui_theme2_k9mail"
+
+    flavorDimensions += "version"
+
+    productFlavors {
+        create("mos") {
+            dimension = "version"
+            isDefault = true
+        }
+    }
 }
 
 dependencies {
--- core/ui/legacy/theme2/k9mail/build.gradle.kts
+++ core/ui/legacy/theme2/k9mail/build.gradle.kts
@@ -4,6 +4,15 @@ plugins {
 
 android {
     namespace = "app.k9mail.core.ui.legacy.theme2.k9mail"
+
+    flavorDimensions += "version"
+
+    productFlavors {
+        create("mos") {
+            dimension = "version"
+            isDefault = true
+        }
+    }
 }
 
 dependencies {
